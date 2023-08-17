63ffb90bb9a666af0a517c5fe0301153fd5d52c8
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 20:58:40 -0700

    Add project files.

---
+++ Interface assignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interf
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        double basicsal, hra, ta, pf, gs, totalsal;
+
+        public Employee(int id, string name, double basicsal)
+        {
+            this.id = id;
+            this.name = name;
+            this.basicsal = basicsal;
+        }
+        public void Calculatesal()
+        {
+            hra = basicsal * 40 / 100;
+            ta = basicsal * 20 / 100;
+            pf = basicsal * 12 / 100;
+            totalsal = basicsal + hra + ta + pf;
+            gs = (basicsal + hra + ta) - pf;
+        }
+
+        public string ToString()
+        {
+            return ($"grosssalary={gs}\nempid={id}\nempname={name}\nbasicsal={basicsal}");
+        }
+    }
+}
---
+++ Interface assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interf
+{
+    internal class Employee1
+    {
+        Employee emp = new Employee(10, "qwe", 20000);
+
+
+
+    }
+}
+
